dc660c63acdda24c93ef9cc35d6825e55120793f
Remco de Boer <remco@ihep.ac.cn>
2020-03-12 09:08:16 +0100

    refactor: clean up YAML test script

--- examples/test.py
+++ examples/test.py
@@ -1,11 +1,18 @@
+"""How to great an intensity tree from a YAML file using TensorWaves"""
 
-import xmltodict
-from yaml import load
+import yaml
 from tensorwaves.physics.helicityformalism.amplitude import create_intensity
 
-with open("examples/intensity-recipe.yaml") as fc:
-    decay_info = load(fc.read())
-    intensity = create_intensity(decay_info)
-    print(intensity)
-    print(intensity({'test': [0.1, 0.2, 1.0, 2.0, 1.5]}))
-    intensity.summary()
+
+def main():
+    """Main function"""
+    with open("examples/intensity-recipe.yaml") as yaml_file:
+        decay_info = yaml.load(yaml_file.read(), Loader=yaml.FullLoader)
+        intensity = create_intensity(decay_info)
+        print(intensity)
+        print(intensity({'test': [0.1, 0.2, 1.0, 2.0, 1.5]}))
+        intensity.summary()
+
+
+if __name__ == "__main__":
+    main()
