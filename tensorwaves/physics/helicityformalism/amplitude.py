0f9620a3c5181f6a2c71dfb30e56e932e563b3fd
Stefan Pflueger <spfluege@gmail.com>
2020-04-28 18:27:43 +0200

    fix: solve issues related to pylint and flake8
    This also includes docstrings.

--- tensorwaves/physics/helicityformalism/amplitude.py
+++ tensorwaves/physics/helicityformalism/amplitude.py
@@ -1,42 +1,79 @@
-from .kinematics import SubSystem
+"""Amplitude module for the helicity formalism.
+
+Its responsibility is the construction of complicated helicity formalism
+amplitude models using the a recipe (see `IntensityBuilder`). These models
+are encapsulated in an `IntensityTF` class, which can be evaluated as a
+regular callable.
+"""
+
+import logging
+import typing
+
+import amplitf.interface as atfi
+from amplitf.dynamics import (relativistic_breit_wigner)
+from amplitf.kinematics import (wigner_capital_d)
+
+import tensorflow as tf
 
 from tensorwaves.interfaces import Function
 
-from amplitf.kinematics import (wigner_capital_d)
-from amplitf.dynamics import (
-    relativistic_breit_wigner)
-import amplitf.interface as atfi
-
-import tensorflow as tf
-import logging
+from .kinematics import (HelicityKinematics, SubSystem)
 
 
 class IntensityTF(Function):
-    def __init__(self, tf_model, parameters):
+    """Implementation of the `~.Function` interface using tensorflow.
+
+    Initialize the intensity based on a tensorflow model.
+
+    Args:
+        tf_model: A callable with potential tensorflow code.
+        parameters: A dict with parameter names as keys and
+            :class:`tf.Variable` as values.
+
+    """
+
+    def __init__(self, tf_model: typing.Callable, parameters: dict):
         self._model = tf_model
         self.__parameters = parameters
 
-    def enable_caching(self):
-        pass
-
     def __call__(self, dataset):
+        """Evaluate the Intensity.
+
+        Args:
+            dataset: A dict of kinematic variables.
+
+        Returns:
+            `numpy.ndarray`: List of intensity values.
+        """
         # it is crucial to convert the input data to tensors
         # otherwise the memory footprint can increase dramatically
         newdataset = {x: tf.constant(y)
                       for x, y in dataset.items()}
-        return self._model(newdataset)
+        return self._model(newdataset).numpy()
 
     @property
-    def parameters(self):
+    def parameters(self) -> dict:
         return {x: y.value().numpy() for x, y in self.__parameters.items()}
 
-    def update_parameters(self, new_parameters):
+    def update_parameters(self, new_parameters: dict):
         for name, value in new_parameters.items():
             self.__parameters[name].assign(value)
 
 
 class IntensityBuilder:
-    def __init__(self, particles, kinematics, phsp_data=None):
+    """Builds Intensities from helicity formalism recipe files.
+
+    Args:
+        particles: A dictionary containing info of various particles.
+        kinematics: A helicity kinematics instance.
+        phsp_data: An optional phase space event collection, which is
+            required if a normalization of the Intensity is performed.
+    """
+
+    def __init__(self,
+                 particles: dict,
+                 kinematics: HelicityKinematics,
+                 phsp_data: dict = None):
         self._particles = particles
         self._dynamics = {}
         self._kinematics = kinematics
@@ -44,6 +81,16 @@ class IntensityBuilder:
         self._phsp_data = phsp_data
 
     def create_intensity(self, recipe: dict):
+        """Create an `IntensityTF` instance based on a recipe.
+
+        Args:
+            recipe: A dict containing builder instructions. These recipe
+                files can be generated via the expert system
+                (see `~.expertsystem.amplitude.helicitydecay.HelicityAmplitudeGeneratorXML`)
+
+        Returns:
+            IntensityTF
+        """
         self._dynamics = recipe['Dynamics']
         if 'Intensity' not in recipe:
             logging.error(
@@ -58,18 +105,25 @@ class IntensityBuilder:
         logging.debug("creating %s", intensity_class)
 
         # this dict could be used for builder injections later on
-        known_intensity_builders = {
+        basic_intensity_builders = {
             'IncoherentIntensity': _IncoherentIntensity,
             'CoherentIntensity': _CoherentIntensity,
             'StrengthIntensity': _StrengthIntensity,
+        }
+        intensity_kinematics_builders = {
             'NormalizedIntensity': _NormalizedIntensity,
         }
-        if intensity_class in known_intensity_builders:
-            return known_intensity_builders[intensity_class](recipe,
-                                                             self,
-                                                             self._kinematics)
-        else:
-            logging.error("unknown intensity %s! Skipping", intensity_class)
+
+        if intensity_class in basic_intensity_builders:
+            return basic_intensity_builders[intensity_class](recipe,
+                                                             self)
+        if intensity_class in intensity_kinematics_builders:
+            return intensity_kinematics_builders[intensity_class](
+                recipe,
+                self,
+                self._kinematics)
+
+        raise Exception("Unknown intensity {}!".format(intensity_class))
 
     def _create_amplitude(self, recipe: dict):
         amplitude_class = recipe['Class']
@@ -82,18 +136,19 @@ class IntensityBuilder:
         general_amplitude_builders = {
             'HelicityDecay': _HelicityDecay,
         }
+
         if amplitude_class in basic_amplitude_builders:
             return basic_amplitude_builders[amplitude_class](recipe,
                                                              self)
-        elif amplitude_class in general_amplitude_builders:
+        if amplitude_class in general_amplitude_builders:
             return general_amplitude_builders[amplitude_class](
                 recipe,
                 self,
                 self._kinematics,
                 self._particles,
                 self._dynamics)
-        else:
-            logging.error("unknown intensity %s! Skipping", amplitude_class)
+
+        raise Exception("Unknown amplitude {}!".format(amplitude_class))
 
     def _register_parameter(self, name: str, value: float):
         if name not in self._parameters:
@@ -129,28 +184,28 @@ class _NormalizedIntensity():
         self._norm_volume = atfi.const(kinematics.phase_space_volume)
 
     @tf.function
-    def __call__(self, x):
+    def __call__(self, dataset):
         normalization = tf.multiply(self._norm_volume,
                                     tf.reduce_mean(
                                         self._model_phsp(self._norm_dataset))
                                     )
-        return tf.divide(self._model_data(x), normalization)
+        return tf.divide(self._model_data(dataset), normalization)
 
 
 class _StrengthIntensity():
-    def __init__(self, recipe: dict, builder, kinematics):
+    def __init__(self, recipe: dict, builder):
         if isinstance(recipe, dict):
             self.strength = builder._get_parameter(recipe['Strength'])
             self.intensity = builder._create_intensity(recipe['Intensity'])
 
-    def __call__(self, x):
-        return self.strength * self.intensity(x)
+    def __call__(self, dataset):
+        return self.strength * self.intensity(dataset)
 
 
 class _IncoherentIntensity():
-    def __init__(self, recipe: dict, builder, kinematics):
+    def __init__(self, recipe: dict, builder):
         if isinstance(recipe['Intensities'], list):
-            intensity_recipes = [v for v in recipe['Intensities']]
+            intensity_recipes = list(recipe['Intensities'])
             self.intensities = [builder._create_intensity(
                 x) for x in intensity_recipes]
         else:
@@ -158,25 +213,25 @@ class _IncoherentIntensity():
                 "Incoherent Intensity requires a list of intensities!"
             )
 
-    def __call__(self, x):
-        return tf.math.accumulate_n([y(x) for y in self.intensities])
+    def __call__(self, dataset):
+        return tf.math.accumulate_n([y(dataset) for y in self.intensities])
 
 
 class _CoherentIntensity():
-    def __init__(self, recipe: dict, builder, kinematics):
+    def __init__(self, recipe: dict, builder):
         if isinstance(recipe['Amplitudes'], list):
-            amp_recipes = [v for v in recipe['Amplitudes']]
+            amp_recipes = list(recipe['Amplitudes'])
             self.amps = [builder._create_amplitude(x) for x in amp_recipes]
         else:
             raise Exception(
                 "Coherent Intensity requires a list of intensities!"
             )
 
-    def __call__(self, x):
+    def __call__(self, dataset):
         return tf.pow(
             tf.cast(
                 tf.abs(
-                    tf.add_n([amp(x) for amp in self.amps])
+                    tf.add_n([amp(dataset) for amp in self.amps])
                 ),
                 dtype=tf.float64),
             tf.constant(2.0, dtype=tf.float64)
@@ -192,18 +247,18 @@ class _CoefficientAmplitude():
 
             self.amp = builder._create_amplitude(recipe['Amplitude'])
 
-    def __call__(self, x):
+    def __call__(self, dataset):
         coefficient = atfi.complex(
             self.mag * atfi.cos(self.phase),
             self.mag * atfi.sin(self.phase)
         )
-        return coefficient * self.amp(x)
+        return coefficient * self.amp(dataset)
 
 
 class _SequentialAmplitude():
     def __init__(self, recipe: list, builder):
         if isinstance(recipe['Amplitudes'], list):
-            amp_recipes = [v for v in recipe['Amplitudes']]
+            amp_recipes = list(recipe['Amplitudes'])
             if len(amp_recipes) == 0:
                 raise Exception("Sequential Amplitude requires a non-empty"
                                 " list of amplitudes!")
@@ -213,16 +268,11 @@ class _SequentialAmplitude():
                 "Sequential Amplitude requires a list of amplitudes!"
             )
 
-    def __call__(self, x):
+    def __call__(self, dataset):
         seq_amp = tf.complex(atfi.const(1.0), atfi.const(0.0))
         for amp in self.seq_amp:
-            seq_amp = tf.multiply(seq_amp, amp(x))
+            seq_amp = tf.multiply(seq_amp, amp(dataset))
         return seq_amp
-
-
-def _non_dynamic(m2, mr, gamma):
-    return tf.complex(tf.constant(1.0, dtype=tf.float64),
-                      tf.constant(0.0, dtype=tf.float64))
 
 
 class _HelicityDecay():
@@ -265,20 +315,23 @@ class _HelicityDecay():
                                   "name " + decaying_state['Name'])
             decay_dynamics = decay_dynamics[0]
 
+            non_dynamics_label = 'nonDynamic'
             known_dynamics_functions = {
                 'relativisticBreitWigner': relativistic_breit_wigner,
-                'nonDynamic': _non_dynamic,
             }
 
-            if decay_dynamics['Type'] not in known_dynamics_functions:
-                raise ValueError(
-                    "Dynamics (" + decay_dynamics['Type'] + ") unknown. " +
-                    "Use one of the following: \n"
-                    + str(known_dynamics_functions.keys()))
+            if non_dynamics_label in decay_dynamics['Type']:
+                self._call_wrapper = self._without_dynamics
             else:
+                if decay_dynamics['Type'] not in known_dynamics_functions:
+                    raise ValueError(
+                        "Dynamics (" + decay_dynamics['Type'] + ") unknown. "
+                        + "Use one of the following: \n"
+                        + str(known_dynamics_functions.keys()))
                 self._dynamics_function = known_dynamics_functions[
                     decay_dynamics['Type']
                 ]
+                self._call_wrapper = self._with_dynamics
 
             # TODO: optionally register other parameters, e.g. the interaction
             # radius (scale for barrier factors)
@@ -291,13 +344,13 @@ class _HelicityDecay():
                 particle_infos['Width']['Value'])
 
             # calculate spin based infos
-            self._J = particle_infos['QuantumNumbers']['Spin']
+            self._j = particle_infos['QuantumNumbers']['Spin']
             self._m = decaying_state['Helicity']
-            self._mprime = (decay_products[0]['Helicity'] -
-                            decay_products[1]['Helicity'])
+            self._mprime = (decay_products[0]['Helicity']
+                            - decay_products[1]['Helicity'])
 
             # TODO: implement canonical formalism and set L
-            self._L = self._J
+            self._l = self._j
 
             # register decay product invariant masses
             self._inv_mass_name_prod1 = kinematics.register_invariant_mass(
@@ -307,9 +360,18 @@ class _HelicityDecay():
         else:
             raise Exception("Helicity Decay expects a dictionary recipe!")
 
-    def __call__(self, x):
-        return wigner_capital_d(x[self._phi_name], x[self._theta_name], 0.0,
-                                2 * self._J, 2 * self._m, 2 * self._mprime) \
-            * self._dynamics_function(x[self._inv_mass_name],
+    def __call__(self, dataset):
+        self._call_wrapper(dataset)
+
+    def _with_dynamics(self, dataset):
+        return wigner_capital_d(dataset[self._phi_name],
+                                dataset[self._theta_name], 0.0,
+                                2 * self._j, 2 * self._m, 2 * self._mprime) \
+            * self._dynamics_function(dataset[self._inv_mass_name],
                                       self._resonance_mass,
                                       self._resonance_width)
+
+    def _without_dynamics(self, dataset):
+        return wigner_capital_d(dataset[self._phi_name],
+                                dataset[self._theta_name], 0.0,
+                                2 * self._j, 2 * self._m, 2 * self._mprime)
