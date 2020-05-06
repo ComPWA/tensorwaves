ebaa174cb1e6911e47cc8b6e81f13b73a3837ebd
Stefan Pflueger <spfluege@gmail.com>
2020-05-06 17:04:30 +0200

    fix: fix test.py and add missing package file

--- examples/test.py
+++ examples/test.py
@@ -1,14 +1,9 @@
 import yaml
 
 from tensorwaves.data.generate import generate_data, generate_phsp
-from tensorwaves.data.tf_phasespace import (
-    TFPhaseSpaceGenerator,
-    TFUniformRealNumberGenerator,
-)
-from tensorwaves.physics.particle import load_particle_list
 from tensorwaves.physics.helicityformalism.amplitude import IntensityBuilder
 from tensorwaves.physics.helicityformalism.kinematics import HelicityKinematics
-
+from tensorwaves.physics.particle import load_particle_list
 
 JPSI_MASS = 3.096900
 PI0_MASS = 0.1349766
@@ -24,7 +19,7 @@ with open("examples/intensity-recipe.yaml") as fc:
     builder = IntensityBuilder(part_list, kin, phsp_sample)
     intensity = builder.create_intensity(recipe)
 
-    data_sample = generate_data(30000, intensity, kin)
+    data_sample = generate_data(30000, kin, intensity)
 
     dataset = kin.convert(data_sample)
 
---
+++ tensorwaves/data/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["generate", "tf_phasespace"]
+
+from . import generate
+from . import tf_phasespace
