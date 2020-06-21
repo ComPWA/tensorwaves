e880a75edd9d7b0a99768a3f51451bc9cc5715b9
Remco de Boer <redeboer@gmx.com>
2020-06-21 12:53:33 +0200

    style: fix pydocstyle D100 and D104

--- tensorwaves/__init__.py
+++ tensorwaves/__init__.py
@@ -1,3 +1,8 @@
+"""A fully featured Partial Wave Analysis framework in Python.
+
+For more info see `tensorwaves.rtfd.io <http://tensorwaves.rtfd.io/>`_.
+"""
+
 __all__ = [
     "estimator",
     "optimizer",
--- tensorwaves/physics/__init__.py
+++ tensorwaves/physics/__init__.py
@@ -1,6 +1,9 @@
 """Evaluateable physics models for amplitude analysis."""
 
-__all__ = ["helicityformalism", "particle"]
+__all__ = [
+    "helicityformalism",
+    "particle",
+]
 
 from . import helicityformalism
 from . import particle
