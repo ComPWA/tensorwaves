883224ac5937a21bf980e112d863859b8ba7abda
Remco de Boer <redeboer@gmx.com>
2020-06-23 16:29:22 +0200

    test: verify extract_particles

--- tests/recipe/test_amplitude_creation.py
+++ tests/recipe/test_amplitude_creation.py
@@ -1,14 +1,27 @@
+from tensorwaves.physics.particle import extract_particles
+
 from . import (
     create_recipe,
     open_recipe,
 )
 
 
-def test_helicity():
+def _create_helicity_recipe():
     recipe_filename = "tests/amplitude_model_heli.yml"
     create_recipe(recipe_filename, formalism="helicity")
     recipe = open_recipe(recipe_filename)
-    assert len(recipe) == 5
+    return recipe
+
+
+class TestRecipeHelicity:
+    recipe = _create_helicity_recipe()
+
+    def test_particle_list(self):
+        particle_list = extract_particles(self.recipe)
+        assert set(particle_list) == {"J/psi", "f0(980)", "gamma", "pi0"}
+
+    def test_helicity(self):
+        assert len(self.recipe) == 5
 
 
 def test_canonical():
