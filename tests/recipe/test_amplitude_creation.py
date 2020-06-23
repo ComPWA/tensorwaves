b59b3bf371224f265c9a2b9655ee8d2b50d64530
Remco de Boer <redeboer@gmx.com>
2020-06-23 16:51:51 +0200

    test: generate phsp sample

--- tests/recipe/test_amplitude_creation.py
+++ tests/recipe/test_amplitude_creation.py
@@ -1,3 +1,4 @@
+from tensorwaves.data.generate import generate_phsp
 from tensorwaves.physics.helicityformalism.kinematics import HelicityKinematics
 from tensorwaves.physics.particle import extract_particles
 
@@ -16,6 +17,7 @@ def _create_helicity_recipe():
 
 class TestRecipeHelicity:
     recipe = _create_helicity_recipe()
+    number_of_phsp_events = 10
 
     def test_particle_list(self):
         particle_list = extract_particles(self.recipe)
@@ -27,6 +29,15 @@ class TestRecipeHelicity:
         masses_fs = kinematics.reaction_kinematics_info.final_state_masses
         assert masses_is == [3.096900]
         assert masses_fs == [0.0, 0.1349766, 0.1349766]
+
+    def generate_phsp(self):
+        kinematics = HelicityKinematics.from_recipe(self.recipe)
+        phsp_sample = generate_phsp(self.number_of_phsp_events, kinematics)
+        return phsp_sample
+
+    def test_phsp(self):
+        phsp_sample = self.generate_phsp()
+        assert phsp_sample.shape == (3, self.number_of_phsp_events, 4)
 
     def test_helicity(self):
         assert len(self.recipe) == 5
