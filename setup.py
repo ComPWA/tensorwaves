f0bb047422b6e00f3e566406e2dc79c05db790c6
Remco de Boer <redeboer@gmx.com>
2020-06-30 11:45:37 +0200

    build: add expertsystem to requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,19 @@ DATA_FILES = [
 ]
 
 
+INSTALL_REQUIRES = [
+    "amplitf==0.0a1",
+    "expertsystem==0.1.2a0",
+    "iminuit",
+    "numpy",
+    "phasespace",
+    "progress",
+    "pyyaml",
+    "sympy",
+    "tensorflow==2.1.*",
+]
+
+
 def long_description() -> str:
     """Parse long description from readme."""
     with open("README.md", "r") as readme_file:
@@ -27,14 +40,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="GPLv3 or later",
     python_requires=">=3.6, <3.8",
-    install_requires=[
-        "amplitf",
-        "numpy",
-        "progress",
-        "pyyaml",
-        "sympy",
-        "tensorflow>=2.1,<2.2",
-    ],
+    install_requires=INSTALL_REQUIRES,
     package_data={"tensorwaves": DATA_FILES},
     include_package_data=True,
 )
