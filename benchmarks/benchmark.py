fcc5bcdeb998c99c742467f6f5606df2183bf907
Remco de Boer <redeboer@gmx.com>
2020-07-01 15:55:29 +0200

    chore: move benchmark procedure to top

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -36,7 +36,7 @@ LOGGER.setLevel(logging.ERROR)
 
 
 SCRIPT_DIR = dirname(realpath(__file__))
-RECIPE_FILE = f"{SCRIPT_DIR}/intensity-recipe.yaml"
+RECIPE_FILE = f"{SCRIPT_DIR}/intensity-recipe.yml"
 
 INITIAL_STATE = [("J/psi")]
 FINAL_STATE = [("gamma"), ("pi0"), ("pi0")]
@@ -46,6 +46,30 @@ FORMALISM_TYPE: str = "helicity"
 
 NUMBER_OF_PHSP_EVENTS = 3e5
 NUMBER_OF_DATA_EVENTS = 3e4
+
+
+def run_benchmark() -> None:
+    create_recipe(
+        initial_state=INITIAL_STATE,
+        final_state=FINAL_STATE,
+        allowed_intermediate_particles=INTERMEDIATE_STATES,
+        recipe_file_name=RECIPE_FILE,
+        formalism_type=FORMALISM_TYPE,
+    )
+    kinematics = create_kinematics(RECIPE_FILE)
+    phsp_sample = generate_phsp_sample(RECIPE_FILE, NUMBER_OF_PHSP_EVENTS)
+    intensity = create_intensity(RECIPE_FILE, phsp_sample)
+    data_sample = generate_data_sample(
+        recipe_file_name=RECIPE_FILE,
+        number_of_events=NUMBER_OF_DATA_EVENTS,
+        kinematics=kinematics,
+        intensity=intensity,
+    )
+    data_set = kinematics.convert(data_sample)
+    estimator = UnbinnedNLL(intensity, data_set)
+    initial_parameters = estimator.parameters
+    minuit2 = Minuit2()
+    minuit2.optimize(estimator, initial_parameters)
 
 
 def create_recipe(
@@ -116,30 +140,6 @@ def generate_data_sample(
     kinematics = create_kinematics(recipe_file_name)
     data_sample = generate_data(int(number_of_events), kinematics, intensity)
     return data_sample
-
-
-def run_benchmark() -> None:
-    create_recipe(
-        initial_state=INITIAL_STATE,
-        final_state=FINAL_STATE,
-        allowed_intermediate_particles=INTERMEDIATE_STATES,
-        recipe_file_name=RECIPE_FILE,
-        formalism_type=FORMALISM_TYPE,
-    )
-    kinematics = create_kinematics(RECIPE_FILE)
-    phsp_sample = generate_phsp_sample(RECIPE_FILE, NUMBER_OF_PHSP_EVENTS)
-    intensity = create_intensity(RECIPE_FILE, phsp_sample)
-    data_sample = generate_data_sample(
-        recipe_file_name=RECIPE_FILE,
-        number_of_events=NUMBER_OF_DATA_EVENTS,
-        kinematics=kinematics,
-        intensity=intensity,
-    )
-    data_set = kinematics.convert(data_sample)
-    estimator = UnbinnedNLL(intensity, data_set)
-    initial_parameters = estimator.parameters
-    minuit2 = Minuit2()
-    minuit2.optimize(estimator, initial_parameters)
 
 
 if __name__ == "__main__":
