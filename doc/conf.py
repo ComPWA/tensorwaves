c72fbf7d7360026469183d3b9a442c5858e38b0c
Remco de Boer <redeboer@gmx.com>
2020-08-28 15:47:01 +0200

    fix: myst-parser warning

--- doc/conf.py
+++ doc/conf.py
@@ -143,3 +143,6 @@ nbsphinx_execute_arguments = [
     "--InlineBackend.figure_formats={'svg', 'pdf'}",
     "--InlineBackend.rc={'figure.dpi': 96}",
 ]
+
+# Settings for myst-parser
+myst_update_mathjax = False
