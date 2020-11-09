b782ce98c3ed6ce268479b30ef74d09e199348c1
Remco de Boer <redeboer@gmx.com>
2020-11-09 12:51:14 +0100

    test: use expertsystem facade functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,12 @@
+import expertsystem as es
 import pytest
-from expertsystem import io
-from expertsystem.amplitude.canonical_decay import CanonicalAmplitudeGenerator
-from expertsystem.amplitude.helicity_decay import HelicityAmplitudeGenerator
 from expertsystem.amplitude.model import AmplitudeModel
 from expertsystem.particle import ParticleCollection
-from expertsystem.reaction import InteractionTypes, StateTransitionManager
 
 
 @pytest.fixture(scope="session")
 def pdg() -> ParticleCollection:
-    return io.load_pdg()
+    return es.io.load_pdg()
 
 
 @pytest.fixture(scope="session")
@@ -28,25 +25,13 @@ def canonical_model() -> AmplitudeModel:
 
 
 def __create_model(formalism: str) -> AmplitudeModel:
-    stm = StateTransitionManager(
-        initial_state=[("J/psi(1S)")],
-        final_state=[("gamma"), ("pi0"), ("pi0")],
+    result = es.generate_transitions(
+        initial_state=("J/psi(1S)", [-1, +1]),
+        final_state=["gamma", "pi0", "pi0"],
         allowed_intermediate_particles=["f(0)(980)"],
         formalism_type=formalism,
         topology_building="isobar",
+        allowed_interaction_types=["EM", "strong"],
         number_of_threads=1,
     )
-
-    stm.set_allowed_interaction_types([InteractionTypes.Strong])
-    graph_interaction_settings_groups = stm.prepare_graphs()
-    result = stm.find_solutions(graph_interaction_settings_groups)
-
-    if formalism == "helicity":
-        amplitude_generator = HelicityAmplitudeGenerator()
-    elif formalism == "canonical-helicity":
-        amplitude_generator = CanonicalAmplitudeGenerator()
-    else:
-        raise NotImplementedError(f"No amplitude generator for {formalism}")
-
-    amplitude_model = amplitude_generator.generate(result)
-    return amplitude_model
+    return es.generate_amplitudes(result)
