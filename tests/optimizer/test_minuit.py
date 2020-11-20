655461b245e2119abebddaa7a22dbd561add75fb
Remco de Boer <redeboer@gmx.com>
2020-11-20 18:59:44 +0100

    test: parameter errors

--- tests/optimizer/test_minuit.py
+++ tests/optimizer/test_minuit.py
@@ -23,7 +23,10 @@ class TestMinuit2:
             "time",
         }
         par_values = result["parameter_values"]
+        par_errors = result["parameter_errors"]
         assert set(par_values) == set(free_pars)
         assert pytest.approx(result["log_likelihood"]) == -13379.223862030514
         assert pytest.approx(par_values["Width_f(0)(500)"]) == 0.55868526502471
+        assert pytest.approx(par_errors["Width_f(0)(500)"]) == 0.01057804923356
         assert pytest.approx(par_values["Mass_f(0)(980)"]) == 0.990141023090767
+        assert pytest.approx(par_errors["Mass_f(0)(980)"]) == 0.000721352674347
