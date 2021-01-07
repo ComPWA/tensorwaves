76394b7688a0b6c92ca877a420eefc7c8212ce9a
Remco de Boer <redeboer@gmx.com>
2021-01-07 21:08:22 +0100

    test: check mass values in data set

--- tests/physics/helicity_formalism/test_kinematics.py
+++ tests/physics/helicity_formalism/test_kinematics.py
@@ -1,10 +1,16 @@
 # pylint: disable=no-self-use
 
 import numpy as np
+from expertsystem.amplitude.model import AmplitudeModel
 
 
 class TestHelicityKinematics:
-    def test_convert(self, data_sample: np.ndarray, data_set: dict):
+    def test_convert(
+        self,
+        helicity_model: AmplitudeModel,
+        data_sample: np.ndarray,
+        data_set: dict,
+    ):
         assert set(data_set) == {
             "mSq_2",
             "mSq_2+3+4",
@@ -18,12 +24,14 @@ class TestHelicityKinematics:
         }
         _, sample_size, _ = data_sample.shape
         assert sample_size == 10000
+        final_state = helicity_model.kinematics.final_state
         float_only_variables = {
-            "mSq_2",
-            "mSq_3",
-            "mSq_4",
+            "mSq_2": final_state[2].mass ** 2,
+            "mSq_3": final_state[3].mass ** 2,
+            "mSq_4": final_state[4].mass ** 2,
         }
-        for var_name, array in data_set.items():
+        for var_name, value in data_set.items():
             if var_name in float_only_variables:
-                continue
-            assert len(array) == sample_size
+                assert value == float_only_variables[var_name]
+            else:
+                assert len(value) == sample_size
