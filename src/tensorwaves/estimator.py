585c6f87649b35d3a0dd92dbb3a919b7bfb67c9a
Stefan Pflueger <spfluege@gmail.com>
2021-03-02 17:54:28 +0100

    chore: remove old Unbinned Log Likelihood

--- src/tensorwaves/estimator.py
+++ src/tensorwaves/estimator.py
@@ -1,88 +1,15 @@
-# pylint: disable=import-outside-toplevel
-
 """Defines estimators which estimate a model's ability to represent the data.
 
 All estimators have to implement the `~.interfaces.Estimator` interface.
 """
 from typing import Callable, Dict, List, Union
 
-import sympy
-import tensorflow as tf
-
-from tensorwaves.interfaces import Estimator, Function
+from tensorwaves.interfaces import Estimator
 from tensorwaves.physics.amplitude import (
     SympyModel,
     get_backend_modules,
     lambdify,
 )
-
-
-class _NormalizedFunction(Function):
-    def __init__(
-        self,
-        unnormalized_function: Function,
-        norm_dataset: dict,
-        norm_volume: float = 1.0,
-    ) -> None:
-        self._model = unnormalized_function
-        # it is crucial to convert the input data to tensors
-        # otherwise the memory footprint can increase dramatically
-        self._norm_dataset = {
-            x: tf.constant(y) for x, y in norm_dataset.items()
-        }
-        self._norm_volume = norm_volume
-
-    def __call__(self, dataset: dict) -> tf.Tensor:
-        normalization = tf.multiply(
-            self._norm_volume,
-            tf.reduce_mean(self._model(self._norm_dataset)),
-        )
-        return tf.divide(self._model(dataset), normalization)
-
-    @property
-    def parameters(self) -> Dict[str, tf.Variable]:
-        return self._model.parameters
-
-    def update_parameters(self, new_parameters: dict) -> None:
-        self._model.update_parameters(new_parameters)
-
-
-class UnbinnedNLL(Estimator):
-    """Unbinned negative log likelihood estimator.
-
-    Args:
-        model: A model that should be compared to the dataset.
-        dataset: The dataset used for the comparison. The model has to be
-            evaluateable with this dataset.
-        phsp_set: A phase space dataset, which is used for the normalization.
-            The model has to be evaluateable with this dataset. When correcting
-            for the detector efficiency use a phase space sample, that passed
-            the detector reconstruction.
-
-    """
-
-    def __init__(self, model: Function, dataset: dict, phsp_set: dict) -> None:
-        if phsp_set and len(phsp_set) > 0:
-            self.__model: Function = _NormalizedFunction(model, phsp_set)
-        else:
-            self.__model = model
-        self.__dataset = dataset
-
-    def __call__(self, parameters: Dict[str, Union[float, complex]]) -> float:
-        self.__model.update_parameters(parameters)
-        props = self.__model(self.__dataset)
-        logs = tf.math.log(props)
-        log_lh = tf.reduce_sum(logs)
-        return -log_lh.numpy()
-
-    def gradient(
-        self, parameters: Dict[str, Union[float, complex]]
-    ) -> Dict[str, Union[float, complex]]:
-        raise NotImplementedError("Gradient not implemented.")
-
-    @property
-    def parameters(self) -> List[str]:
-        return list(self.__model.parameters.keys())
 
 
 def gradient_creator(
@@ -136,8 +63,7 @@ class SympyUnbinnedNLL(  # pylint: disable=too-many-instance-attributes
         backend_modules = get_backend_modules(backend)
 
         self.__parameters: Dict[str, Union[float, complex]] = {
-            k.name: v.evalf() if isinstance(v, sympy.Expr) else v
-            for k, v in model.parameters.items()
+            k.name: v for k, v in model.parameters.items()
         }
 
         model_expr = model.expression.doit()
