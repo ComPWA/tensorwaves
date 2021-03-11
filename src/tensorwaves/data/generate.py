eec09d80fa2c210b89f7098f8e3b31d076f16b04
Remco de Boer <redeboer@gmx.com>
2021-03-11 12:19:04 +0100

    fix: use tqdm.autonotebook
    See https://github.com/ComPWA/expertsystem/pull/508

--- src/tensorwaves/data/generate.py
+++ src/tensorwaves/data/generate.py
@@ -4,7 +4,7 @@ import logging
 from typing import Callable, Optional
 
 import numpy as np
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 from tensorwaves.data.tf_phasespace import (
     TFPhaseSpaceGenerator,
