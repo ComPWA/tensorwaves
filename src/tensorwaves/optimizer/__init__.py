c3feeaa74dfc7486ab6c939f03d14c5fc4e7f75e
Stefan Pflueger <spfluege@gmail.com>
2021-03-16 18:42:32 +0100

    fix: make scipy and optional dependency

--- src/tensorwaves/optimizer/__init__.py
+++ src/tensorwaves/optimizer/__init__.py
@@ -4,6 +4,13 @@ Optimizers 'fit' a `.Model` to some data sample with regard to `.Estimator` as
 loss function.
 """
 
-__all__ = ["minuit", "scipy", "callbacks"]
+__all__ = ["minuit", "callbacks"]
 
-from . import callbacks, minuit, scipy
+from . import callbacks, minuit
+
+try:
+    from . import scipy  # noqa: F401
+
+    __all__.append("scipy")
+except ImportError:
+    pass
