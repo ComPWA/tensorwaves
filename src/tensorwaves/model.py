09b57b10a13eefe7c8bcc9fcedd94731fb95daa6
Remco de Boer <redeboer@gmx.com>
2021-03-23 11:11:28 +0100

    feat: implement create_intensity_component

--- src/tensorwaves/model.py
+++ src/tensorwaves/model.py
@@ -5,10 +5,20 @@ computational backends. Currently, mathematical expressions are implemented
 as `sympy` expressions only.
 """
 
-from typing import Callable, Dict, FrozenSet, Mapping, Tuple, Union
+from typing import (
+    Callable,
+    Dict,
+    FrozenSet,
+    Iterable,
+    Mapping,
+    Sequence,
+    Tuple,
+    Union,
+)
 
 import numpy as np
 import sympy as sp
+from expertsystem.amplitude.helicity import HelicityModel
 
 from tensorwaves.interfaces import DataSample, Function, Model
 
@@ -173,3 +183,49 @@ class SympyModel(Model):
     @property
     def argument_order(self) -> Tuple[str, ...]:
         return tuple(x.name for x in self.__argument_order)
+
+
+def add_components(
+    model: HelicityModel,
+    components: Union[str, Iterable[str]],
+) -> sp.Expr:
+    """Coherently or incoherently add components of a helicity model."""
+    if isinstance(components, str):
+        components = [components]
+    for component in components:
+        if component not in model.components:
+            raise KeyError(
+                f'Component "{component}" not in model components',
+                list(model.components),
+            )
+    if any(map(lambda c: c.startswith("I"), components)) and any(
+        map(lambda c: c.startswith("A"), components)
+    ):
+        intensity_sum = add_components(
+            model,
+            components=filter(lambda c: c.startswith("I"), components),
+        )
+        amplitude_sum = add_components(
+            model,
+            components=filter(lambda c: c.startswith("A"), components),
+        )
+        return intensity_sum + amplitude_sum
+    if all(map(lambda c: c.startswith("I"), components)):
+        return sum(model.components[c] for c in components)
+    if all(map(lambda c: c.startswith("A"), components)):
+        return abs(sum(model.components[c] for c in components)) ** 2
+    raise ValueError('Not all component names started with either "A" or "I"')
+
+
+def create_intensity_component(
+    model: HelicityModel,
+    components: Union[str, Sequence[str]],
+    backend: str,
+) -> LambdifiedFunction:
+    """Create a `.LambdifiedFunction` of a sum of helicity model components."""
+    added_components = add_components(model, components)
+    sympy_model = SympyModel(
+        expression=added_components,
+        parameters=model.parameters,
+    )
+    return LambdifiedFunction(sympy_model, backend=backend)
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,9 +3,14 @@
 import numpy as np
 import pytest
 import sympy as sp
+from expertsystem.amplitude.helicity import HelicityModel
 
 from tensorwaves.interfaces import DataSample, Function
-from tensorwaves.model import LambdifiedFunction, SympyModel
+from tensorwaves.model import (
+    LambdifiedFunction,
+    SympyModel,
+    create_intensity_component,
+)
 
 
 @pytest.fixture(scope="module")
@@ -71,3 +76,34 @@ def test_canonical(canonical_model: SympyModel):
         "d_f(0)(500)",
         "Gamma_f(0)(500)",
     }
+
+
+def test_create_intensity_component(
+    phsp_set: DataSample,
+    es_helicity_model: HelicityModel,
+    intensity: LambdifiedFunction,
+):
+    # pylint: disable=line-too-long
+    model = es_helicity_model
+    from_amplitudes = create_intensity_component(
+        model,
+        components=[
+            R"A[J/\psi(1S)_{+1} \to f_{0}(500)_{0} \gamma_{+1};f_{0}(500)_{0} \to \pi^{0}_{0} \pi^{0}_{0}]",
+            R"A[J/\psi(1S)_{+1} \to f_{0}(980)_{0} \gamma_{+1};f_{0}(980)_{0} \to \pi^{0}_{0} \pi^{0}_{0}]",
+        ],
+        backend="numpy",
+    )
+    from_intensity = create_intensity_component(
+        model,
+        components=R"I[J/\psi(1S)_{+1} \to \gamma_{+1} \pi^{0}_{0} \pi^{0}_{0}]",
+        backend="numpy",
+    )
+    assert pytest.approx(from_amplitudes(phsp_set)) == from_intensity(phsp_set)
+
+    intensity_components = [
+        create_intensity_component(model, component, backend="numpy")
+        for component in model.components
+        if component.startswith("I")
+    ]
+    sub_intensities = [i(phsp_set) for i in intensity_components]
+    assert pytest.approx(sum(sub_intensities)) == intensity(phsp_set)
