f4c8ec6d3f515381081dd0215b8f32fcf6dfd51e
Remco de Boer <redeboer@gmx.com>
2021-06-24 14:07:38 +0200

    refactor: rename real argument to complex_twice (#297)

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -4,5 +4,5 @@ from tensorwaves.interface import FitResult
 
 class TestFitResult:
     def test_count_number_of_parameters(self, fit_result: FitResult):
-        assert fit_result.count_number_of_parameters(real=False) == 3
-        assert fit_result.count_number_of_parameters(real=True) == 4
+        assert fit_result.count_number_of_parameters(complex_twice=False) == 3
+        assert fit_result.count_number_of_parameters(complex_twice=True) == 4
