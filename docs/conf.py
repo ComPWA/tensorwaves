c1ab1f305374a51bc7c61227caf5d574b54ee117
Remco de Boer <redeboer@gmx.com>
2021-08-30 10:36:42 +0200

    docs: refer to compwa-org for contributing (#309)
    * build: limit black due to typing-extensions conflict
    * ci: upgrade pinned requirements
    * ci: update pin_requirements script
      https://github.com/ComPWA/repo-maintenance/commit/70e1239
    
    Co-authored-by: GitHub <noreply@github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -163,6 +163,7 @@ intersphinx_mapping = {
         f"https://ampform.readthedocs.io/en/{RELEASES['ampform']}/",
         None,
     ),
+    "compwa-org": ("https://compwa-org.readthedocs.io/en/stable", None),
     "iminuit": ("https://iminuit.readthedocs.io/en/stable", None),
     "jax": ("https://jax.readthedocs.io/en/stable", None),
     "matplotlib": ("https://matplotlib.org", None),
