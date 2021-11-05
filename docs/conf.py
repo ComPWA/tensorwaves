3f756fc8379c960b24a28fa701ff8fd77ef8d5f4
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-11-05 14:38:47 +0100

    fix: add missing substitutions plugin

--- docs/conf.py
+++ docs/conf.py
@@ -283,6 +283,7 @@ myst_enable_extensions = [
     "colon_fence",
     "dollarmath",
     "smartquotes",
+    "substitution",
 ]
 BINDER_LINK = f"https://mybinder.org/v2/gh/ComPWA/{REPO_NAME}/{BRANCH}?filepath=docs/usage"
 myst_substitutions = {
