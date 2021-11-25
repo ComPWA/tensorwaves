db1d0f77de60b1c91350ff5648a46deda284b813
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-11-25 16:56:52 +0100

    test: write test for split_expression

--- tests/unit/model/test_sympy.py
+++ tests/unit/model/test_sympy.py
@@ -1,10 +1,11 @@
-# pylint: disable=no-self-use, redefined-outer-name
+# pylint: disable=invalid-name, no-self-use, redefined-outer-name
 import numpy as np
 import pytest
 import sympy as sp
 
 from tensorwaves.interface import DataSample, Function
 from tensorwaves.model import LambdifiedFunction, SympyModel
+from tensorwaves.model.sympy import split_expression
 
 
 class TestLambdifiedFunction:
@@ -48,3 +49,21 @@ class TestLambdifiedFunction:
         np.testing.assert_array_almost_equal(
             results, expected_results, decimal=4
         )
+
+
+def create_expression(x, y, z):
+    return x ** z + 2 * y
+
+
+def test_split_expression():
+    x, y, z = sp.symbols("x y z")
+    expression = create_expression(x, y, z)
+    top_expr, sub_expressions = split_expression(expression, max_complexity=3)
+    assert top_expr.free_symbols == set(sub_expressions)
+    assert expression == top_expr.xreplace(sub_expressions)
+
+    sub_symbols = sorted(top_expr.free_symbols, key=lambda s: s.name)
+    assert len(sub_symbols) == 2
+    f1, f2 = tuple(sub_symbols)
+    assert sub_expressions[f1] == x ** z
+    assert sub_expressions[f2] == 2 * y
