b33c4c3af7a40a6c04e362cf0a16f30a707a5598
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-12-01 21:05:16 +0100

    test: lambdify ComplexSqrt with different backends

---
+++ tests/unit/function/test_ampform.py
@@ -0,0 +1,27 @@
+# pylint: disable=import-outside-toplevel
+import numpy as np
+import pytest
+
+from tensorwaves._backend import find_function
+from tensorwaves.function.sympy import create_parametrized_function
+
+
+@pytest.mark.parametrize("backend", ["jax", "math", "numba", "numpy"])
+def test_complex_sqrt(backend: str):
+    import sympy as sp
+    from ampform.dynamics.math import ComplexSqrt
+    from numpy.lib.scimath import sqrt as complex_sqrt
+
+    x = sp.Symbol("x")
+    expr = ComplexSqrt(x)
+    function = create_parametrized_function(
+        expr.doit(), parameters={}, backend=backend
+    )
+    if backend == "math":
+        values = -4
+    else:
+        linspace = find_function("linspace", backend)
+        values = linspace(-4, +4, 9)
+    data = {"x": values}
+    output_array = function(data)  # type: ignore[arg-type]
+    np.testing.assert_almost_equal(output_array, complex_sqrt(data["x"]))
