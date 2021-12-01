a5340f098ce5f4158cf4f0554879632a9c984ba9
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-12-01 20:59:46 +0100

    test: try more functions in test_find_function

--- tests/unit/test_backend.py
+++ tests/unit/test_backend.py
@@ -8,7 +8,16 @@ def test_find_function():
     import numpy as np
     import tensorflow.experimental.numpy as tnp
 
-    assert find_function("mean", backend="numpy") is np.mean
+    assert find_function("array", backend="numpy") is np.array
+    assert find_function("linspace", backend="numpy") is np.linspace
     assert find_function("log", backend="numpy") is np.log
-    assert find_function("mean", backend="tf") is tnp.mean
+    assert find_function("mean", backend="numpy") is np.mean
+    assert find_function("mean", backend="numba") is np.mean
+
+    assert find_function("array", backend="jax") is jnp.array
+    assert find_function("linspace", backend="jax") is jnp.linspace
     assert find_function("mean", backend="jax") is jnp.mean
+
+    assert find_function("array", backend="tf") is tnp.array
+    assert find_function("linspace", backend="tf") is tnp.linspace
+    assert find_function("mean", backend="tf") is tnp.mean
