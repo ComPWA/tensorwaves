c701a328902c1c7d31c8c556bf0746d05767ac74
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-12-01 17:48:07 +0100

    chore: import TF modules inline
    TF is expensive to import

--- tests/unit/test_backend.py
+++ tests/unit/test_backend.py
@@ -1,13 +1,13 @@
-# pylint: disable=import-error
-# pyright: reportMissingImports=false
-import jax.numpy as jnp
-import numpy as np
-import tensorflow.experimental.numpy as tnp
-
 from tensorwaves._backend import find_function
 
 
 def test_find_function():
+    # pylint: disable=import-error, import-outside-toplevel
+    # pyright: reportMissingImports=false
+    import jax.numpy as jnp
+    import numpy as np
+    import tensorflow.experimental.numpy as tnp
+
     assert find_function("mean", backend="numpy") is np.mean
     assert find_function("log", backend="numpy") is np.log
     assert find_function("mean", backend="tf") is tnp.mean
