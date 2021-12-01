4a69c191808c3758e158b3da95e45cf7c1ea4f4f
Remco de Boer <redeboer@gmx.com>
2021-12-01 16:30:56 +0100

    fix: jit-compile recombined fast_lambdify function (#361)
    * docs: use fast lambdify in usage notebook step 3
    * fix: add remaining symbols to sub_expression dict
    * fix: include min and max in complexity interval
    * fix: jit-compile recombined fast_lambdify function
    * fix: return unsplit expression if max_complexity is 0
    * refactor: extract jit_compile function
    * style: improve function order definition in sympy module

--- src/tensorwaves/function/sympy.py
+++ src/tensorwaves/function/sympy.py
@@ -21,82 +21,10 @@ from sympy.printing.numpy import (
 )
 from tqdm.auto import tqdm
 
-from tensorwaves._backend import get_backend_modules
+from tensorwaves._backend import get_backend_modules, jit_compile
 from tensorwaves.interface import ParameterValue
 
 from . import ParametrizedBackendFunction
-
-_jax_known_functions = {
-    k: v.replace("numpy.", "jnp.") for k, v in _numpy_known_functions.items()
-}
-_jax_known_constants = {
-    k: v.replace("numpy.", "jnp.") for k, v in _numpy_known_constants.items()
-}
-
-
-class _JaxPrinter(NumPyPrinter):  # pylint: disable=abstract-method
-    # pylint: disable=invalid-name
-    module_imports = {"jax": {"numpy as jnp"}}
-    _module = "jnp"
-    _kc = _jax_known_constants
-    _kf = _jax_known_functions
-
-    def _print_ComplexSqrt(self, expr: sp.Expr) -> str:  # noqa: N802
-        x = self._print(expr.args[0])
-        return (
-            "jnp.select("
-            f"[jnp.less({x}, 0), True], "
-            f"[1j * jnp.sqrt(-{x}), jnp.sqrt({x})], "
-            "default=jnp.nan)"
-        )
-
-
-def split_expression(
-    expression: sp.Expr,
-    max_complexity: int,
-    min_complexity: int = 0,
-) -> Tuple[sp.Expr, Dict[sp.Symbol, sp.Expr]]:
-    """Split an expression into a 'top expression' and several sub-expressions.
-
-    Replace nodes in the expression tree of a `sympy.Expr
-    <sympy.core.expr.Expr>` that lie within a certain complexity range (see
-    :meth:`~sympy.core.basic.Basic.count_ops`) with symbols and keep a mapping
-    of each to these symbols to the sub-expressions that they replaced.
-
-    .. seealso:: :doc:`/usage/faster-lambdify`
-    """
-    i = 0
-    symbol_mapping: Dict[sp.Symbol, sp.Expr] = {}
-    n_operations = sp.count_ops(expression)
-    if n_operations < max_complexity:
-        return expression, symbol_mapping
-    progress_bar = tqdm(
-        total=n_operations,
-        desc="Splitting expression",
-        unit="node",
-        disable=not _use_progress_bar(),
-    )
-
-    def recursive_split(sub_expression: sp.Expr) -> sp.Expr:
-        nonlocal i
-        for arg in sub_expression.args:
-            complexity = sp.count_ops(arg)
-            if min_complexity < complexity < max_complexity:
-                progress_bar.update(n=complexity)
-                symbol = sp.Symbol(f"f{i}")
-                i += 1
-                symbol_mapping[symbol] = arg
-                sub_expression = sub_expression.xreplace({arg: symbol})
-            else:
-                new_arg = recursive_split(arg)
-                sub_expression = sub_expression.xreplace({arg: new_arg})
-        return sub_expression
-
-    top_expression = recursive_split(expression)
-    remainder = progress_bar.total - progress_bar.n
-    progress_bar.update(n=remainder)  # pylint crashes if total is set directly
-    progress_bar.close()
-    return top_expression, symbol_mapping
 
 
 def create_parametrized_function(
@@ -129,6 +57,58 @@ def create_parametrized_function(
             symbol.name: value for symbol, value in parameters.items()
         },
     )
+
+
+def lambdify(
+    expression: sp.Expr,
+    symbols: Sequence[sp.Symbol],
+    backend: Union[str, tuple, dict],
+    **kwargs: Any,
+) -> Callable:
+    """A wrapper around :func:`~sympy.utilities.lambdify.lambdify`."""
+    # pylint: disable=import-outside-toplevel, too-many-return-statements
+    def jax_lambdify() -> Callable:
+        return jit_compile(backend="jax")(
+            sp.lambdify(
+                symbols,
+                expression,
+                modules=modules,
+                printer=_JaxPrinter,
+                **kwargs,
+            )
+        )
+
+    def numba_lambdify() -> Callable:
+        return jit_compile(backend="numba")(
+            sp.lambdify(symbols, expression, modules="numpy", **kwargs)
+        )
+
+    def tensorflow_lambdify() -> Callable:
+        # pylint: disable=import-error
+        import tensorflow.experimental.numpy as tnp  # pyright: reportMissingImports=false
+
+        return sp.lambdify(symbols, expression, modules=tnp, **kwargs)
+
+    modules = get_backend_modules(backend)
+    if isinstance(backend, str):
+        if backend == "jax":
+            return jax_lambdify()
+        if backend == "numba":
+            return numba_lambdify()
+        if backend in {"tensorflow", "tf"}:
+            return tensorflow_lambdify()
+
+    if isinstance(backend, tuple):
+        if any("jax" in x.__name__ for x in backend):
+            return jax_lambdify()
+        if any("numba" in x.__name__ for x in backend):
+            return numba_lambdify()
+        if any(
+            "tensorflow" in x.__name__ or "tf" in x.__name__ for x in backend
+        ):
+            return tensorflow_lambdify()
+
+    return sp.lambdify(symbols, expression, modules=modules, **kwargs)
 
 
 def fast_lambdify(
@@ -167,6 +147,7 @@ def fast_lambdify(
         sub_function = lambdify(sub_expression, symbols, backend, **kwargs)
         sub_functions.append(sub_function)
 
+    @jit_compile(backend)  # type: ignore[arg-type]
     def recombined_function(*args: Any) -> Any:
         new_args = [sub_function(*args) for sub_function in sub_functions]
         return top_function(*new_args)
@@ -174,64 +155,80 @@ def fast_lambdify(
     return recombined_function
 
 
-def lambdify(
+def split_expression(
     expression: sp.Expr,
-    symbols: Sequence[sp.Symbol],
-    backend: Union[str, tuple, dict],
-    **kwargs: Any,
-) -> Callable:
-    """A wrapper around :func:`~sympy.utilities.lambdify.lambdify`."""
-    # pylint: disable=import-outside-toplevel,too-many-return-statements
-    def jax_lambdify() -> Callable:
-        import jax
+    max_complexity: int,
+    min_complexity: int = 1,
+) -> Tuple[sp.Expr, Dict[sp.Symbol, sp.Expr]]:
+    """Split an expression into a 'top expression' and several sub-expressions.
 
-        return jax.jit(
-            sp.lambdify(
-                symbols,
-                expression,
-                modules=modules,
-                printer=_JaxPrinter,
-                **kwargs,
-            )
-        )
+    Replace nodes in the expression tree of a `sympy.Expr
+    <sympy.core.expr.Expr>` that lie within a certain complexity range (see
+    :meth:`~sympy.core.basic.Basic.count_ops`) with symbols and keep a mapping
+    of each to these symbols to the sub-expressions that they replaced.
 
-    def numba_lambdify() -> Callable:
-        # pylint: disable=import-error
-        import numba
+    .. seealso:: :doc:`/usage/faster-lambdify`
+    """
+    i = 0
+    symbol_mapping: Dict[sp.Symbol, sp.Expr] = {}
+    n_operations = sp.count_ops(expression)
+    if max_complexity <= 0 or n_operations < max_complexity:
+        return expression, symbol_mapping
+    progress_bar = tqdm(
+        total=n_operations,
+        desc="Splitting expression",
+        unit="node",
+        disable=not _use_progress_bar(),
+    )
 
-        return numba.jit(
-            sp.lambdify(symbols, expression, modules="numpy", **kwargs),
-            forceobj=True,
-            parallel=True,
-        )
+    def recursive_split(sub_expression: sp.Expr) -> sp.Expr:
+        nonlocal i
+        for arg in sub_expression.args:
+            complexity = sp.count_ops(arg)
+            if min_complexity <= complexity <= max_complexity:
+                progress_bar.update(n=complexity)
+                symbol = sp.Symbol(f"f{i}")
+                i += 1
+                symbol_mapping[symbol] = arg
+                sub_expression = sub_expression.xreplace({arg: symbol})
+            else:
+                new_arg = recursive_split(arg)
+                sub_expression = sub_expression.xreplace({arg: new_arg})
+        return sub_expression
 
-    def tensorflow_lambdify() -> Callable:
-        # pylint: disable=import-error
-        import tensorflow.experimental.numpy as tnp  # pyright: reportMissingImports=false
-
-        return sp.lambdify(symbols, expression, modules=tnp, **kwargs)
-
-    modules = get_backend_modules(backend)
-    if isinstance(backend, str):
-        if backend == "jax":
-            return jax_lambdify()
-        if backend == "numba":
-            return numba_lambdify()
-        if backend in {"tensorflow", "tf"}:
-            return tensorflow_lambdify()
-
-    if isinstance(backend, tuple):
-        if any("jax" in x.__name__ for x in backend):
-            return jax_lambdify()
-        if any("numba" in x.__name__ for x in backend):
-            return numba_lambdify()
-        if any(
-            "tensorflow" in x.__name__ or "tf" in x.__name__ for x in backend
-        ):
-            return tensorflow_lambdify()
-
-    return sp.lambdify(symbols, expression, modules=modules, **kwargs)
+    top_expression = recursive_split(expression)
+    remaining_symbols = top_expression.free_symbols - set(symbol_mapping)
+    symbol_mapping.update({s: s for s in remaining_symbols})
+    remainder = progress_bar.total - progress_bar.n
+    progress_bar.update(n=remainder)  # pylint crashes if total is set directly
+    progress_bar.close()
+    return top_expression, symbol_mapping
 
 
 def _use_progress_bar() -> bool:
     return logging.getLogger().level <= logging.WARNING
+
+
+_jax_known_functions = {
+    k: v.replace("numpy.", "jnp.") for k, v in _numpy_known_functions.items()
+}
+_jax_known_constants = {
+    k: v.replace("numpy.", "jnp.") for k, v in _numpy_known_constants.items()
+}
+
+
+class _JaxPrinter(NumPyPrinter):  # pylint: disable=abstract-method
+    # pylint: disable=invalid-name
+    module_imports = {"jax": {"numpy as jnp"}}
+    _module = "jnp"
+    _kc = _jax_known_constants
+    _kf = _jax_known_functions
+
+    def _print_ComplexSqrt(self, expr: sp.Expr) -> str:  # noqa: N802
+        x = self._print(expr.args[0])
+        return (
+            "jnp.select("
+            f"[jnp.less({x}, 0), True], "
+            f"[1j * jnp.sqrt(-{x}), jnp.sqrt({x})], "
+            "default=jnp.nan)"
+        )
--- tests/unit/function/test_sympy.py
+++ tests/unit/function/test_sympy.py
@@ -1,5 +1,6 @@
 # cspell:ignore lambdifygenerated
 import sys
+from typing import Tuple
 
 import numpy as np
 import pytest
@@ -8,35 +9,37 @@ import sympy as sp
 from tensorwaves.function.sympy import fast_lambdify, split_expression
 
 
-def create_expression(x, y, z):
-    return x ** z + 2 * y
+def create_expression(a, x, y, z) -> sp.Expr:
+    return a * (x ** z + 2 * y)
 
 
 @pytest.mark.parametrize("backend", ["jax", "math", "numpy", "tf"])
-@pytest.mark.parametrize("max_complexity", [2, 3, 4])
+@pytest.mark.parametrize("max_complexity", [0, 1, 2, 3, 4, 5])
 def test_fast_lambdify(backend: str, max_complexity: int):
-    x, y, z = sp.symbols("x y z")
-    expression = create_expression(x, y, z)
+    symbols: Tuple[sp.Symbol, ...] = sp.symbols("a x y z")
+    a, x, y, z = symbols
+    expression = create_expression(a, x, y, z)
     function = fast_lambdify(
         expression,
-        symbols=[x, y, z],
-        max_complexity=max_complexity,
+        symbols,
         backend=backend,
+        max_complexity=max_complexity,
     )
 
     func_repr = str(function)
-    if max_complexity <= 3:
-        assert func_repr.startswith("<function fast_lambdify.<locals>")
+    if 0 < max_complexity <= 4:
+        repr_start = "<function fast_lambdify.<locals>"
     else:
         repr_start = "<function _lambdifygenerated"
-        if backend == "jax":
-            if sys.version_info >= (3, 7):
-                repr_start = "<CompiledFunction of " + repr_start
-            else:
-                repr_start = "<CompiledFunction object at 0x"
-        assert func_repr.startswith(repr_start)
+    if backend == "jax":
+        if sys.version_info >= (3, 7):
+            repr_start = "<CompiledFunction of " + repr_start
+        else:
+            repr_start = "<CompiledFunction object at 0x"
+    assert func_repr.startswith(repr_start)
 
     data = (
+        4,
         np.array([1, 2]),
         np.array([1, np.e]),
         np.array([1, 2]),
@@ -47,14 +50,29 @@ def test_fast_lambdify(backend: str, max_complexity: int):
 
 
 def test_split_expression():
-    x, y, z = sp.symbols("x y z")
-    expression = create_expression(x, y, z)
-    top_expr, sub_expressions = split_expression(expression, max_complexity=3)
+    symbols: Tuple[sp.Symbol, ...] = sp.symbols("a x y z")
+    a, x, y, z = symbols
+    expression = create_expression(a, x, y, z)
+
+    assert expression.args[0] is a
+    assert len(expression.args[1].args) == 2
+    sub_expr, _ = expression.args[1].args
+    assert sub_expr == x ** z
+    n_nodes = sp.count_ops(sub_expr)
+    assert n_nodes == 1
+
+    top_expr, sub_expressions = split_expression(
+        expression,
+        min_complexity=n_nodes,
+        max_complexity=n_nodes,
+    )
     assert top_expr.free_symbols == set(sub_expressions)
     assert expression == top_expr.xreplace(sub_expressions)
 
     sub_symbols = sorted(top_expr.free_symbols, key=lambda s: s.name)
-    assert len(sub_symbols) == 2
-    f1, f2 = tuple(sub_symbols)
+    assert len(sub_symbols) == 3
+    f0, f1, f2 = tuple(sub_symbols)
+    assert f0 is a
+    assert sub_expressions[f0] == a
     assert sub_expressions[f1] == x ** z
     assert sub_expressions[f2] == 2 * y
