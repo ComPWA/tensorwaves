6e7ccc7ca353524f6d3e6a14e1896b1499a17efd
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-12-08 13:56:43 +0100

    fix: ignore link to JAX GPU installation

--- docs/conf.py
+++ docs/conf.py
@@ -258,6 +258,9 @@ copybutton_prompt_text = r">>> |\.\.\. "  # doctest
 linkcheck_anchors = False
 if os.environ.get("CI", False):
     linkcheck_anchors = True
+    linkcheck_anchors_ignore = [
+        r"pip\-installation\-gpu\-cuda",
+    ]
 
 # Settings for myst_nb
 execution_timeout = -1
