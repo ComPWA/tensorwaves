ecb57a0a87b127545b52a4ae1ead5ec73d5a0f48
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-01-03 13:48:15 +0100

    chore: combine stack/concat events implementations

--- src/tensorwaves/data/_data_sample.py
+++ src/tensorwaves/data/_data_sample.py
@@ -1,6 +1,6 @@
 """Helper functions for modifying `.DataSample` instances."""
 
-from typing import Any
+from typing import Any, Callable, Sequence
 
 import numpy as np
 from tqdm.auto import tqdm
@@ -15,6 +15,18 @@ def get_number_of_events(four_momenta: DataSample) -> int:
 
 
 def concatenate_events(sample1: DataSample, sample2: DataSample) -> DataSample:
+    return _merge_events(sample1, sample2, merge_method=np.concatenate)
+
+
+def stack_events(sample1: DataSample, sample2: DataSample) -> DataSample:
+    return _merge_events(sample1, sample2, merge_method=np.vstack)
+
+
+def _merge_events(
+    sample1: DataSample,
+    sample2: DataSample,
+    merge_method: Callable[[Sequence[np.ndarray]], np.ndarray],
+) -> DataSample:
     if len(sample1) and len(sample2) and set(sample1) != set(sample2):
         raise ValueError(
             "Keys of data sets are not matching", set(sample2), set(sample1)
@@ -22,18 +34,8 @@ def concatenate_events(sample1: DataSample, sample2: DataSample) -> DataSample:
     if get_number_of_events(sample1) == 0:
         return sample2
     return {
-        i: np.concatenate((array, sample2[i])) for i, array in sample1.items()
+        i: merge_method((array, sample2[i])) for i, array in sample1.items()
     }
-
-
-def stack_events(sample1: DataSample, sample2: DataSample) -> DataSample:
-    if len(sample1) and len(sample2) and set(sample1) != set(sample2):
-        raise ValueError(
-            "Keys of data sets are not matching", set(sample2), set(sample1)
-        )
-    if get_number_of_events(sample1) == 0:
-        return sample2
-    return {i: np.vstack((array, sample2[i])) for i, array in sample1.items()}
 
 
 def select_events(four_momenta: DataSample, selector: Any) -> DataSample:
