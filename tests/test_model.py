0d54c14e31c5ee8885126b110f43247a1ae97ef4
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-06-08 19:39:59 +0200

    tests: remove test_sympy_performance_optimization()

--- tests/test_model.py
+++ tests/test_model.py
@@ -74,23 +74,3 @@ def test_helicity(helicity_model: SympyModel):
         "m_f(0)(500)",
         "Gamma_f(0)(500)",
     }
-
-
-@pytest.mark.parametrize(
-    ("parameters", "variables", "backend"),
-    [
-        ({"c_1": 1 + 1j}, {"x": np.array([1, 2, 3])}, "numpy"),
-        ({"c_1": 1 + 1j}, {"x": np.array([1, 2, 3])}, "jax"),
-    ],
-)
-def test_sympy_performance_optimization(
-    parameters: dict, variables: dict, backend: str, sympy_model, function
-) -> None:
-    function.update_parameters(parameters)
-    expected_values = function(variables)
-    opt_model = sympy_model.performance_optimize(
-        fix_inputs={**parameters, **variables}
-    )
-    callable_model = LambdifiedFunction(opt_model, backend)
-
-    np.testing.assert_almost_equal(callable_model({}), expected_values)
