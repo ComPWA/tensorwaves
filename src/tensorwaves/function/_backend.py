edd55dba62a00bfe5ca8623a5bc7cae2e6ea2c10
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-11-06 12:17:44 +0100

    MAINT: limit scope of pyright ignore statements

--- src/tensorwaves/function/_backend.py
+++ src/tensorwaves/function/_backend.py
@@ -49,9 +49,8 @@ def get_backend_modules(backend: str | tuple | dict) -> str | tuple | dict:
         if backend in {"tensorflow", "tf"}:
             try:
                 # pylint: disable=import-error, no-name-in-module
-                # pyright: reportMissingImports=false
                 import tensorflow as tf
-                import tensorflow.experimental.numpy as tnp
+                import tensorflow.experimental.numpy as tnp  # pyright: ignore[reportMissingImports]
                 from tensorflow.python.ops.numpy_ops import np_config
             except ImportError:  # pragma: no cover
                 raise_missing_module_error("tensorflow", extras_require="tf")
