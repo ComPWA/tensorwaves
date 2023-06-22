827fa1b705256edbe4db5c280b05eaaac21612cf
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-06-22 10:04:56 +0200

    DOC: update link to Minuit

--- src/tensorwaves/optimizer/minuit.py
+++ src/tensorwaves/optimizer/minuit.py
@@ -18,7 +18,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Minuit2(Optimizer):
-    """Adapter to `Minuit2 <https://root.cern/doc/master/md_math_minuit2_doc_Minuit2.html#Minuit2Page>`_.
+    """Adapter to `Minuit2 <https://root.cern.ch/root/htmldoc/guides/minuit2/Minuit2.html>`_.
 
     Implements the `~.interface.Optimizer` interface using `iminuit.Minuit`.
 
