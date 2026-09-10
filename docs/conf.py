cf2c5b07bef302b98f3cc1e7d0a28d48280c8ba7
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-09-10 11:57:38 +0200

    MAINT: remove redundant `RemovedInSphinx10Warning`

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,7 @@
 import os
 import subprocess
-import warnings
 
 import requests
-from sphinx.deprecation import RemovedInSphinx10Warning
 from sphinx_api_relink.helpers import (
     get_branch_name,
     get_execution_mode,
@@ -12,8 +10,6 @@ from sphinx_api_relink.helpers import (
     pin_minor,
     set_intersphinx_version_remapping,
 )
-
-warnings.filterwarnings("ignore", category=RemovedInSphinx10Warning)
 
 
 def create_tensorflow_inventory() -> None:
